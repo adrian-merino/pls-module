44ad39f2ca7f33e73b30188f17d003ae6799ca7f
Adrian Merino <adrian.fernando.merino@ut.ee>
2025-06-21 10:36:05 +0200

    doc: add more guides to funcs

--- pls_module/plsmodule.py
+++ pls_module/plsmodule.py
@@ -19,6 +19,10 @@ class PLSModeler:
         )
     
 class IPLSModeler:
+    """ Class for filtering data for samples or wavenumbers to be removed
+
+    :param file_name: file path of .csv file
+    """
     def __init__ (self, file_path=0):
         self.file_path = file_path
 
@@ -43,6 +47,7 @@ class IPLSModeler:
         self.results_list = []
 
     def organize_data(self):
+        """Organizes data read for further transformation and modelling"""
         self.data_organizer.open_file()
         self.data_organizer.org_data()
         self.org_xblock = self.data_organizer.give_xblock()
@@ -53,6 +58,10 @@ class IPLSModeler:
         
 
     def do_preprocessing(self, preproc_list):
+        """ Func for triggering preprocessing of data
+
+        :param preproc_list: list of preprocessing methods for transforming data set, list
+        """
         if preproc_list:
             self.preproc_list = preproc_list
             self.preprocessor.load_data(self.working_xblock)
@@ -60,16 +69,28 @@ class IPLSModeler:
 
     # for setting up lists
     def pick_preproc(self, picked_preproc):
+        """ Func for accepting preprocessing methods to be used
+
+        :param picked_preproc: list of preprocessing methods to be used, list
+        """
         self.preproc_list = picked_preproc
         return
         
     def select_region(self, region_list):
+        """ Func for accepting wavenumber regions to be used
+
+        :param region_list: list of lists containing start and end wavenumbers of regions of interest, list
+        """
         if region_list:
             self.regions_list = region_list
             self.data_filterer.pick_regions(self.regions_list)
             self.working_xblock = self.data_filterer.give_xblock()
     
     def remove_point(self, points_list):
+        """ Func for accepting samples to be removed
+
+        :param points_list: list of samples names to be excluded, list
+        """
         if points_list:
             self.pts_list = points_list
             self.data_filterer.remove_samples(self.pts_list, True)
@@ -78,7 +99,11 @@ class IPLSModeler:
 
     # main funcs
     def get_ipls_settings(self):
+        """ Func for creating a list of all possible settings for modelling
 
+        :return: list of all possible configurations of wavenumbers, preproc methods, and samples for modelling
+        :rtype: list
+        """
         # Check all combinations of removed points
         for pt_num in range (0, len(self.pts_list)+1):
             for pt_comb in combinations(self.pts_list, pt_num):
@@ -93,6 +118,11 @@ class IPLSModeler:
         return self.settings_list
 
     def get_ipls_results(self):
+        """ Func for creating PLS models for each configuration in the settings list
+
+        :return: list containing performance metrics of each created model, along with its setting
+        :rtype: list
+        """
         for settings in self.settings_list:
             self.organize_data()
             self.select_region(settings[1])
--- pls_module/spectra_parser/dataselector.py
+++ pls_module/spectra_parser/dataselector.py
@@ -1,27 +1,52 @@
 from pandas import pd
 
 class DataSelector:
+    """Class for filtering data for samples or wavenumbers to be removed"""
     def __init__(self):
         self.working_xblock = None
         self.working_yblock = None
 
     def load_data(self, xblock, yblock):
+        """ Loads data to DataSelector object
+
+        :param xblock: table of variable values, dataframe
+        :param yblock: table of response values, dataframe
+        """
         self.working_xblock = xblock
         self.working_yblock = yblock
 
     def remove_samples(self, samples_list, touch_y=False):
+        """ Remove samples from provided list
+
+        :param samples_list: samples to be excluded from the calibration data, list
+        :param touch_y: indicator if y-block will have its samples removed as well, bool
+        """
         self.working_xblock = self.working_xblock.drop(list(samples_list))
         if touch_y:
             self.working_yblock = self.working_yblock.drop(list(samples_list))
     
     def pick_regions(self, regions_list):
+        """ Remove regions of wavenumbers from provided list
+
+        :param regions_list: list containing lists of start and end of wavenumber region to be removed, list
+        """
         df_list = []
         for region in regions_list:
             df_list.append(self.working_xblock.loc[:, region[0]:region[1]])
         self.working_xblock = pd.concat(df_list, axis=1)
 
     def give_xblock(self):
+        """ Returns filtered x-block
+
+        :return: x-block
+        :rtype: dataframe
+        """
         return self.working_xblock
 
     def give_yblock(self):
+        """ Returns filtered y-block
+
+        :return: y-block
+        :rtype: dataframe
+        """
         return self.working_yblock
