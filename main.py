c7c21984bf8de3e147903490599abc1b57e89cd8
Adrian Merino <adrian.fernando.merino@ut.ee>
2025-06-21 13:02:59 +0200

    feat: add print for max r2

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ def list_pls_models_test(file_path, disable_warnings=False):
     a.pick_preproc(["SG", "SNV", "MC",])
 
     a.get_ipls_settings()
-    a.get_ipls_results()
+    results = a.get_ipls_results()
+
+    max_val = max([res[0] for res in results])
+    best_settings = results[[res[0] for res in results].index(max_val)][1]
+    print(f"max r2 is {max_val} with settings of {best_settings}")
 
 if __name__ == "__main__":
     list_pls_models_test("tests/test_data.csv", True)
