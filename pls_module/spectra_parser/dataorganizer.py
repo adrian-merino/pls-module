807acd275a77fc4b6f49c1f136906d1e0764dffd
Adrian Merino <adrian.fernando.merino@ut.ee>
2025-06-21 10:19:06 +0200

    doc: add guide on dataorganizer class

--- pls_module/spectra_parser/dataorganizer.py
+++ pls_module/spectra_parser/dataorganizer.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 class DataOrganizer:
+    """Class for organizing data
+
+    :param file_name: file path of .csv file
+    """
     def __init__(self, file_name):
         self.file_path = file_name
         self.raw_df = None
@@ -10,10 +14,17 @@ class DataOrganizer:
         self.y_block = None
 
     def open_file(self):
+        """ Opens a .csv file and convert it into a pandas dataframe
+        """
         self.raw_df = pd.read_csv(self.file_path)
         self.raw_df = self.raw_df.loc[self.raw_df['Gum Product'] == 795]
 
     def org_data(self):
+        """ Organizes data, ready for further transformation or modelling
+
+        :return: None, message for missing data
+        :rtype: None, str
+        """
         if not all(self.raw_df):
             return "no data found"
         else:
@@ -26,21 +37,41 @@ class DataOrganizer:
             self.absorbance_list = self.trimmed_df.iloc[0, :].to_list() # just getting one sample here
 
     def give_abs(self):
+        """ Give the absorbance values of spectral data
+
+        :return: list of absorbance values
+        :rtype: list
+        """
         if not all(self.absorbance_list):
             return "no data found"
         return self.absorbance_list
 
     def give_wn(self):
+        """ Give the wavenumber values of spectral data
+
+        :return: list of wavenumber values
+        :rtype: list
+        """
         if not all(self.wavenumbers_list):
             return "no data found"
         return self.wavenumbers_list
 
     def give_xblock(self):
+        """ Give the organized values of the variable block
+
+        :return: organized x-block
+        :rtype: dataframe
+        """
         if not all(self.trimmed_df):
             return "no data found"
         return self.trimmed_df
 
     def give_yblock(self, mode="normal"):
+        """ Give the organized values of the response block
+
+        :return: organized yblock
+        :rtype: dataframe
+        """
         if mode == "test":
             return [1]*25 + [5]*25 + [0.2]*25 + [100]*25 # len(test_y) this is sample y-block data only
         else:
